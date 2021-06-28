2de466345dcacb620c06a44719fb83dc58c752b7
TetianaZelena <tanydans@gmail.com>
2021-06-28 22:36:09 +0300

    chenged js add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-import '@fortawesome/fontawesome-free/js/brands.js';
-import '@fortawesome/fontawesome-free/js/solid.js';
-import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import 'modern-normalize/modern-normalize.css';
 import menuEvents from './menu.json';
 import eventsTemplates from './templates/events.hbs';
@@ -19,29 +16,35 @@ const Theme = {
 
 const swichToggleTheme = document.querySelector('.js-theme-switch__toggle');
 const bodyTheme = document.querySelector('body');
+upCurrentdarkTheme();
+upCurrentLightTheme();
 let isLightTheme = true;
 swichToggleTheme.addEventListener('change', (e) => {
    const savedTheme = e.target.value;
+   e.preventDefault();
+   
    if (isLightTheme) {
       //light
       bodyTheme .classList.remove('dark-theme');
       bodyTheme.classList.add('light-theme');
       localStorage.removeItem('dark-theme', savedTheme);
-      localStorage.setItem('light-theme', savedTheme )
+      upCurrentLightTheme();
+      localStorage.setItem('light-theme', savedTheme);
    }
    else {
       //dark
       bodyTheme .classList.remove('light-theme');
       bodyTheme.classList.add('dark-theme');
       localStorage.removeItem('light-theme', savedTheme);
+      upCurrentdarkTheme();
       localStorage.setItem('dark-theme', savedTheme)
    }
    isLightTheme = !isLightTheme;
 });
 
-// function saveCurrentTheme() {
-//    const onSaveCurentTheme = localStorage.getItem('dark-theme')
-//    if (onSaveCurentTheme) {
-//       bodyTheme.value = onSaveCurentTheme;
-//    }
-//    }
+function upCurrentdarkTheme() {
+   bodyTheme.value = localStorage.getItem('dark-theme');  
+};
+   function upCurrentLightTheme() {
+   bodyTheme.value = localStorage.getItem('light-theme');  
+};
